9604e1f11390ca398b663262523c97e6ec68ca37
liuh <yfdream@163.com>
2018-05-23 18:07:37 +0800

    修改默认的 waitstrategy，

--- src/main/java/com/example/demo/disruptor/DisruptorProducerFactory.java
+++ src/main/java/com/example/demo/disruptor/DisruptorProducerFactory.java
@@ -1,12 +1,14 @@
 package com.example.demo.disruptor;
 
 
+import com.lmax.disruptor.LiteTimeoutBlockingWaitStrategy;
 import com.lmax.disruptor.WaitStrategy;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
 
 public class DisruptorProducerFactory {
     private static final int MAXIMUM_CAPACITY = 1 << 30;
@@ -28,7 +30,7 @@ public class DisruptorProducerFactory {
     }
 
     public static <T> DisruptorProducer<T> create(int bufferSize, ProducerType producerType) {
-        return create(bufferSize, getDefaultThreadFactory(), producerType, new YieldingWaitStrategy());
+        return create(bufferSize, getDefaultThreadFactory(), producerType, getDefaultWaitStrategy());
     }
 
     public static <T> DisruptorProducer<T> create(int bufferSize, ProducerType producerType, WaitStrategy waitStrategy) {
@@ -37,15 +39,15 @@ public class DisruptorProducerFactory {
 
     public static <T> DisruptorProducer<T> create(int bufferSize, ThreadFactory threadFactory, ProducerType producerType, WaitStrategy waitStrategy) throws IllegalArgumentException {
         if (bufferSize < 16) {
-            throw new IllegalArgumentException("bufferSize min size 16， must be the nth power of 2");
+            throw new IllegalArgumentException("bufferSize最小16， 且为2的N次方");
         }
 
         if (null == threadFactory) {
-            throw new IllegalArgumentException("threadFactory is null");
+            throw new IllegalArgumentException("threadFactory为空");
         }
 
         if (null == waitStrategy) {
-            throw new IllegalArgumentException("waitStrategy is null");
+            throw new IllegalArgumentException("waitStrategy为空");
         }
 
         //转换成2的n次方，大于等于原来的值,如果超过最大值，就设置成最大值
@@ -59,6 +61,10 @@ public class DisruptorProducerFactory {
                 waitStrategy);
 
         return new DisruptorProducer<>(disruptor);
+    }
+
+    private static WaitStrategy getDefaultWaitStrategy() {
+        return new LiteTimeoutBlockingWaitStrategy(100, TimeUnit.MILLISECONDS);
     }
 
     private static ThreadFactory getDefaultThreadFactory() {
